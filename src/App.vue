6dbc0d2797c724269b493b4b8a7fff8bd8a1f634
Chiaravalle04 <nicolas.chiaravalle@gmail.com>
2023-04-20 15:38:45 +0200

    Inizializzato progetto BOOLBNB FRONT-END

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: "App",
+  
+  components: {
+    AppHeader
+  },
+};
+</script>
+
+<template>
+
+  <AppHeader/>
+
+  <router-view></router-view>
+  
+</template>
+
+<style lang="scss">
+@import './style/app.scss';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "AppHeader",
+
+  components: {
+
+  },
+
+  data() {
+
+    return {
+
+    }
+
+  },
+};
+</script>
+
+<template>
+
+  <h1 class="text-danger">ciao</h1>
+  
+</template>
+
+<style lang="scss" scoped>
+
+</style>
