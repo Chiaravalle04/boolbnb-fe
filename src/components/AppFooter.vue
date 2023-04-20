8bb92aa65f82c0af3f723f812a5f3fa8bd73edfc
Fedois <fedefox14@gmail.com>
2023-04-20 16:42:50 +0200

    riempito contenitori nel footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -21,11 +21,49 @@ export default {
   <footer>
     <div class="container-fluid d-flex justify-content-between border py-2">
       <div class="content-dx">
-        ciao
+        <ul class="d-flex p-0 m-0">
+          <li>
+            &copy; 2023 Boolbnb, inc.
+          </li>
+          <li>
+            <a href="">
+              Federico Fois
+            </a>
+          </li>
+          <li>
+            <a href="">
+              Francesco Benvenuti
+            </a>
+          </li>
+          <li>
+            <a href="">
+              Nicolas Chiaravalle
+            </a>
+          </li>
+          <li>
+            <a href="">
+              Fabiana Aveta
+            </a>
+          </li>
+        </ul>
       </div>
 
       <div class="content-sx">
-        ciao
+        <ul class="d-flex p-0 m-0">
+          <li>
+            &ClockwiseContourIntegral;Italiano (IT)
+          </li>
+          <li>
+            <a href="">
+              &euro; EUR
+            </a>
+          </li>
+          <li>
+            <a href="">
+              Supporto e risorse &UpArrow;
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   </footer>
@@ -33,5 +71,13 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+li{
+  list-style: none;
+  margin-right: 10px;
 
+  a{
+    color: black;
+    text-decoration: none;
+  }
+}
 </style>
