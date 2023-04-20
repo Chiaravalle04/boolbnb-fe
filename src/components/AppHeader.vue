0ac86723fdc9694dd77996d3c5623e33ca9b0438
Francesco98Welcome <frankbenvenuti04@gmail.com>
2023-04-20 16:51:09 +0200

    messo font awesome

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,23 +17,19 @@ export default {
 </script>
 
 <template>
-
   <div class="container">
-      <div class="row">
-          <div class="col-3">
-              <img src="https://www.digital.ink/wp-content/uploads/airbnb_logo_detail.jpg" class="w-100" alt="">
-          </div>
-          <div class="col-6">
-              <h1 class="text-danger text-center">Parte centrale</h1>
-          </div>
-          <div class="col-3">
-              <h1 class="text-danger text-end">Zona Login</h1>
-          </div>
+    <div class="row">
+      <div class="col-3">
+        <img src="https://www.digital.ink/wp-content/uploads/airbnb_logo_detail.jpg" class="w-100" alt="">
       </div>
+      <div class="col-6">
+        <h1 class="text-danger text-center">Parte centrale <i class="fa-solid fa-house"></i></h1>
+      </div>
+      <div class="col-3">
+        <h1 class="text-danger text-end">Zona Login</h1>
+      </div>
+    </div>
   </div>
-  
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
