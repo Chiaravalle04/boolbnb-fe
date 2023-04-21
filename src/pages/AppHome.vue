8ffbb333e459bbb80303e79432ff41a8a81384fd
Ludovico Longhini <ludovico.longhini@gmail.com>
2023-04-21 11:08:35 +0200

    MODALE

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -60,6 +60,27 @@ export default {
                             </button>
                         </div>
                     </div>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Apri il modal
+                    </button>
+
+                    <div class="modal" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                    Nich fattelo a mano stronzo
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                                    <button type="button" class="btn btn-primary">Salva le modifiche</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
